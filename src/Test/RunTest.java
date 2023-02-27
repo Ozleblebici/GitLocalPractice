45c48b0b51970620d7700e5352af1ea6d5b6d81e
Talha Ozleblebici <talha43gs@gmail.com>
2023-02-27 19:27:57 +0000

    Cemal Updates

--- src/Test/RunTest.java
+++ src/Test/RunTest.java
@@ -3,6 +3,8 @@ package Test;
 public class RunTest {
 
     public static void main(String[] args) {
-        System.out.println("TEST");
+        System.out.println("TEST_remote");
+        // bla bla bla
+        // bla bla bla
     }
 }
