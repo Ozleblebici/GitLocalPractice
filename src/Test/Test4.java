a3aa8c777cb621be3c7022632f067d70387ea471
Talha <talha43gs@gmail.com>
2023-02-27 17:25:40 +0000

    Test4 Updated

--- src/Test/Test4.java
+++ src/Test/Test4.java
@@ -2,7 +2,7 @@ package Test;
 
 public class Test4 {
     public static void main(String[] args) {
-        System.out.println("TEST");
+        System.out.println("INVALID TEST");
 
         System.out.println("Test2");
     }
