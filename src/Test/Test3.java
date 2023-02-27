c1d54bf729643deaa1e31bb04a305dd416b748d4
Talha <talha43gs@gmail.com>
2023-02-27 19:48:42 +0000

    PR Fix

--- src/Test/Test3.java
+++ src/Test/Test3.java
@@ -2,8 +2,7 @@ package Test;
 
 public class Test3 {
     // static genel verify method
+    // static genel verify method  ek methdo silindi bu kapsiyor
     // static genel verify method
-    // static genel verify method
-    // A101 testleri eklendi
     // A101 testleri eklendi
 }
